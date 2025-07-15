6107029013cfdb4e05e3c6f6abf5f9b56eda07be
Bean Boonyawat <bean1888@outlook.com>
2025-07-15 15:27:47 +0700

    feat: pinia

---
+++ src/views/PiniaPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Pinia {{ counterStore.count }}</h1>
+  <button v-on:click="counterStore.increment()"> add number</button>
+</template>
+
+<script setup lang="ts">
+  import { useCounterStore } from '@/stores/counter'
+  
+  const counterStore = useCounterStore()
+</script>
+
+<style scoped>
+
+</style>
