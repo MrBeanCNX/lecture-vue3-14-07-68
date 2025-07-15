18cdfd46fce6047cbf9d5a3ee2b0bd4541105b66
Bean Boonyawat <bean1888@outlook.com>
2025-07-15 15:27:58 +0700

    feat: composable

---
+++ src/views/ComposablePage.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Pinia {{ counterStore.count }}</h1>
+  <button v-on:click="counterStore.increment()"> add number</button>
+
+  <h1>Composable {{ counter.count }}</h1>
+  <button v-on:click="counter.increment()"> add number</button>
+</template>
+
+<script setup lang="ts">
+  import { useCounter } from '@/composables/useCounter'
+  import { useCounterStore } from '@/stores/counter'
+  
+  const counterStore = useCounterStore()
+  const counter = useCounter()
+</script>
+
+<style scoped>
+
+</style>
